b757a9c6b00048ef87dc908a61546f8d141c4dcd
Vaibhav Gupta <vaibhav.gupta@hexaviewtech.com>
2021-09-10 15:14:18 +0530

    to B2

---
+++ prac2.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+
+provider "aws" {
+  region = "ap-southeast-1"
+  shared_credentials_file = "/home/vaibhavg/.aws/credentials"
+ }
+
+
+resource "aws_instance" "tfdemoec2" {
+  ami           = "ami-0d058fe428540cd89"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tfec2"
+  }
+}
