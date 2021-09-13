e0e7ab85da10c4fe6811e6622f7d3dce495ccbc0
Vaibhav Gupta <vaibhav.gupta@hexaviewtech.com>
2021-09-13 20:16:03 +0530

    f

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform{
     backend "s3"{
-    bucket = "tfbucket-hexa"
-    region = "ap-southeast-1"
-    key = "global/s3/terraform.tfstate"
-     #shared_credentials_file = var.credentials_loc
+      bucket = "tfbucket-hexa"
+      region = "ap-southeast-1"
+      key = "/ec2/terraform.tfstate"
   }
 }
