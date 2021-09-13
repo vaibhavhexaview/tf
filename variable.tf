4f716a3b0132ec6c2c137e3ff0b5b8781fb22f38
Vaibhav Gupta <vaibhav.gupta@hexaviewtech.com>
2021-09-13 16:09:45 +0530

    Added force destroy to s3

--- variable.tf
+++ variable.tf
@@ -2,6 +2,10 @@ variable "credentials_loc" {
     type = string
     default = "/home/vaibhavg/.aws/credentials"
 }
+variable "region" {
+  type = string
+  default = "ap-southeast-1"
+}
 
 #Instance Names
 variable "ec2_name" {
